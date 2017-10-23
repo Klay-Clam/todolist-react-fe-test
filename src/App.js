eb919c3c77b456395423c3fd3e54e9d4b6ff956e
Clam <hellodearclam@outlook.com>
2017-10-24 00:25:30 +0800

    做了一些尝试，睡觉咯

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {isToggleOn: true};
+  //   this.openUserBan = this.openUserBan.bind(this);
+  // }
+  // openUserBan() {
+  //   console.log('打开左侧的用户面板');
+  // }
   render() {
     return ( 
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <a href="#"><img src=''/></a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type='text' className=''/>
+        <button id='createItems'>new</button>
       </div>
     );
   }
