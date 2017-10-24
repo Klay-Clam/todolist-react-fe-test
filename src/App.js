b7da1b0a8c4ae5a744198e16cb42f78cc4f37715
Klay-Clam <hellodearclam@outlook.com>
2017-10-24 17:54:48 +0800

    new code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Router, Route, hashHistory } from 'react-router';
 import './App.css';
 
+const routeComponent = function (type, context) {
+  return (
+    <div className="panel panel-default">
+      <div className="panel-body">
+        <List type={type}
+          items={Search(context.state.items, context.state.search)}
+          checkItem={context.checkItem}
+          deleteItem={context.deleteItem}
+          editItem={context.editItem} />
+
+        <Alert value={context.state.alert.value}
+          visible={context.state.alert.visible}
+          hide={() => context.alert("", false, "")}
+          type={context.state.alert.type} />
+      </div>
+
+      <div className="panel-footer">
+        <LeftItems value={context.state.leftCount} />
+        <ListSelector active={type} />
+
+        <GlobalAction
+          deleteCompleted={context.deleteCompleted}
+          checkAll={context.checkAll} />
+      </div>
+    </div>
+  )
+}
+
 class App extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {isToggleOn: true};
-  //   this.openUserBan = this.openUserBan.bind(this);
-  // }
-  // openUserBan() {
-  //   console.log('打开左侧的用户面板');
-  // }
   render() {
     return ( 
-      <div className="App">
-        <header className="App-header">
-          <a href="#"><img src=''/></a>
-        </header>
-        <input type='text' className=''/>
-        <button id='createItems'>new</button>
-      </div>
+      <Router history={hashHistory} key={reloadCounter}>
+        <Route path="/" component={() => routeComponent("all", this)} />
+        <Route path="/active" component={() => routeComponent("active", this)} />
+        <Route path="/completed" component={() => routeComponent("completed", this)} />
+      </Router>
     );
   }
 }
