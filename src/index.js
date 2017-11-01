43ed33d6a9a0c0a2cd4e9b76cf13993c3d12ecde
Klay-Clam <hellodearclam@outlook.com>
2017-11-01 08:55:11 +0800

    删除分支上的不需要的文件

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ToDoList from './containers/ToDoList';
-import './index.css';
-
-ReactDOM.render(
-  <ToDoList />,
-  document.getElementById('root')
-);
--- src/utils/IsBlank.js
+++
@@ -1,9 +0,0 @@
-export function removeSpaces(string) {
-        return string.replace(/\s{2,}/g, ' ');
-}
-
-export function isBlank(string) {
-        string = removeSpaces(string);
-
-        return string !== ' ' && string !== '';
-    }
