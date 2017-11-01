9ac22e0f003fba39af79734b2b67e495077b3df4
Klay-Clam <hellodearclam@outlook.com>
2017-11-01 09:00:15 +0800

    先写一个简单的todolist，只需要基本的功能

--- react-todolist-app/react-todolist-view/src/store/Store.js
+++
@@ -1,7 +0,0 @@
-export function storeGet() {
-    return JSON.parse(localStorage.getItem("Items"));
-}
-
-export function storeSave(items) {
-    localStorage.setItem("Items", JSON.stringify(items));
-}
